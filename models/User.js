218e967b468b738725b9e80e06be78d60f0e87cd
hyunha <fly9739@gmail.com>
2021-10-02 22:37:32 +0900

    2021.10.02 로그인기능

--- models/User.js
+++ models/User.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+
 const userSchema = mongoose.Schema({
     name: {
         type: String,
@@ -45,8 +47,46 @@ userSchema.pre('save', function( next ) {
     } else {
         next()
     }
-
 })
 
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+
+    //plainpassword 와 암호화된 비밀번호가 맞는지 확인
+    bcrypt.compare(plainPassword, this.password, function(err,isMatch) {
+        if(err) return cb(err),
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+    var user = this;
+    //jsonwebtoken을 이용하여 토큰 생성
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    // user._id + 'secretToken' = token
+    // ->
+    // 'secretToken' -> user._id
+    
+    user.token = token
+    user.save(function(err, user) {
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
+
+
+userSchema.statics.findByToken = function(token, cb) {
+    var user = this;
+    user._id + '' = token
+    //토큰을 복호화(decode)함.
+    jwt.verity(token, 'secretToken', function(err, decoded) {
+        //유저 아이디를 이용해서 유저를 찾은 후,
+        //클라이언트에서 가져온 토큰과 DB에 보관된 토큰이 일치하는지 확인
+
+        user.findOne({"_id": decoded, "token" : token}, function (err, user) {
+            if(err) return cb(err);
+            cb(null, user)
+        })
+    })
+}
 const User = mongoose.model('User',userSchema)
 module.exports = { User }
