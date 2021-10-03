c739490527e0ffd4328653fe063baed3fd99a816
hyunha <fly9739@gmail.com>
2021-10-03 11:09:31 +0900

    2021.10.03 로그아웃기능

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.post('/api/users/register', (req, res) => {
 app.post('/api/users/login', (req, res) => {
 //요청된 이메일을 데이터베이스에서 있는지 찾는다.
 User.findOne({ email: req.body.email }, (err, user) => {
-  if(!user) {
+  if (!user) {
     return res.json({
       loginSuccess: false,
       message: "제공된 이메일에 해당하는 유저가 없습니다."
@@ -50,11 +50,12 @@ User.findOne({ email: req.body.email }, (err, user) => {
 //요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는지 확인한다.
 user.comparePassword(req.body.password , (err, isMatch) => {
   if(!isMatch)
-    return res.json({ loginSuccess: false, message:"비밀번호가 틀렸습니다."})
+    return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다."})
 
 //비밀번호가 맞다면 토큰을 생성한다.
 user.generateToken((err, user) => {
   if (err) return res.status(400).send(err);
+
   //토큰 저장하기. (쿠키에 저장)
   res.cookie("x_auth", user.token)
   .status(200)
@@ -81,6 +82,15 @@ res.status(200).json({
 })
 })
 
-
+app.get('/api/users/logout', auth, (req,res) => {
+  User.findOneAndUpdate({_id: req.user._id},
+    {token: ""},
+    (err, user) => {
+      if(err) return res.json({success: false, err});
+      return res.status(200).send({
+        success: ture
+      })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
