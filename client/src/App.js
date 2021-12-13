8d6a13a9161862112b820834d059eb21a4b27da9
hyunha <fly9739@gmail.com>
2021-12-13 20:07:59 +0900

    2021.12.13  auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Auth from "./components/Auth/Auth";
+import Cart from "./components/views/Cart";
 import LandingPage from "./components/views/LandingPage/LandingPage"
 import LoginPage from "./components/views/LoginPage/LoginPage"
 import RegisterPage from "./components/views/RegisterPage/RegisterPage"
 // import Auth  from "./hoc/auth"
+
 function App() {
   return (
     <BrowserRouter>
@@ -11,6 +14,7 @@ function App() {
           <Route exact path="/" element={<LandingPage/>} />
           <Route exact path="/login" element={<LoginPage/>} />
           <Route exact path="/register" element={<RegisterPage/>} />
+          <Route exact path="/cart" element={<Auth><Cart/></Auth>} />
         </Routes>
     </BrowserRouter>
   );
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,36 +1,61 @@
-import React, { useEffect } from 'react'
-import axios from 'axios';
-import { useNavigate,Link } from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { auth } from "../../../_actions/user_action";
 
 function LandingPage(props) {
+  const dispatch = useDispatch();
+  const [isAuth, setisAuth] = useState(false);
 
-    const navigate = useNavigate();
+  dispatch(auth()).then((response) => {
+    setisAuth(response.payload.isAuth);
+  });
+  console.log(isAuth);
 
-    useEffect(() => {
-        axios.get('/api/hello')
-        .then(response => {console.log(response)})
-    }, [])
+  const navigate = useNavigate();
 
-    const onClickLogout = ()=> {
-        axios.get('/api/users/logout')
-        .then(response => {
-            if(response.data){
-                alert("로그아웃성공")
-            navigate("/login")
-            }else{
-                alert("로그아웃실패")
-            }
-        })
-    }
-    return (
-        <div style={{display:'flex',justifyContent:'center',alignItems:'center',width:'100%',height:'100vh'}}>
-            <h2>LandingPage</h2>
-            <Link to="/login">
-            <button>로그인</button>
-            </Link>
-            <button onClick={onClickLogout}>로그아웃</button>
-        </div>
-    )
+  useEffect(() => {
+    axios.get("/api/hello").then((response) => {
+      console.log(response);
+    });
+  }, []);
+
+  const onClickLogout = () => {
+    axios.get("/api/users/logout").then((response) => {
+      if (response.data) {
+        alert("로그아웃성공");
+        navigate("/");
+      } else {
+        alert("로그아웃실패");
+      }
+    });
+  };
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <h2>LandingPage</h2>
+      {isAuth ? (
+        <>
+          <button onClick={onClickLogout}>로그아웃</button>
+          <Link to="/cart">
+            <button>장바구니</button>
+          </Link>
+        </>
+      ) : (
+        <Link to="/login">
+          <button>로그인</button>
+        </Link>
+      )}
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
